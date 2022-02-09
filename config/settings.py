97759940d676d754257c56f1a30f85caf6d2b174
Do Il Jeon <98448316+wjsehdlf77@users.noreply.github.com>
2022-02-10 02:19:04 +0900

    이미지 업로드 프로필사진x

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dailyphoto',
-    'common'
+    'common',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
--- dailyphoto/urls.py
+++ dailyphoto/urls.py
@@ -14,10 +14,13 @@ urlpatterns = [
     path('<int:post_id>/', views.detail), # <int:post_id>/   <- 상세보기 주소
     path('upload/', views.post_create, name="post_create"),
     path('<str:username>/', views.profile, name="profile"),
-    path('modify', views.modify_profile, name="profile_modify"),
+    path('modify', views.modify_profile, name="modify_profile"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+
+
 
 
 
