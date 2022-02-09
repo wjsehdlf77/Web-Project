4cef36f86b71e9537c2c506d9e9fc42e633207d7
Do Il Jeon <98448316+wjsehdlf77@users.noreply.github.com>
2022-02-09 16:14:20 +0900

    커밋커밋

--- dailyphoto/urls.py
+++ dailyphoto/urls.py
@@ -13,7 +13,8 @@ urlpatterns = [
     path('', views.index),
     path('<int:post_id>/', views.detail), # <int:post_id>/   <- 상세보기 주소
     path('upload/', views.post_create),    
-    path('<str:username>/', views.dailyphoto_preview, name="dailyphoto_preview"),
+    path('<str:username>/', views.profile, name="profile"),
+    path('modify', views.modify_profile, name="profile_modify"),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
