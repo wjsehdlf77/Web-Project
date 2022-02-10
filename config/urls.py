8107ae95b67c0d0f7479268ae63e0bd22afd4cc9
Kyeonghui <dkdk177@naver.com>
2022-02-10 16:47:57 +0900

    ../ 수정

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 #from common import views
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +12,5 @@ urlpatterns = [
     path('dailyphoto/',include('dailyphoto.urls'))
 
 ]
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
--- dailyphoto/urls.py
+++ dailyphoto/urls.py
@@ -1,7 +1,5 @@
 
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from . import views
 
 
@@ -16,11 +14,5 @@ urlpatterns = [
     path('<str:username>/', views.profile, name="profile"),
     path('modify', views.modify_profile, name="modify_profile"),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
-
-
-
 
 
