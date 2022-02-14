11ac675088edbb0f5368db26fc48a96d1efd0ef9
JHJ <90052524+quantumleapj@users.noreply.github.com>
2022-02-14 14:44:57 +0900

    프로필 최신순 문제 고쳐짐

--- dailyphoto/views.py
+++ dailyphoto/views.py
@@ -135,7 +135,9 @@ def post_create(request):
 def profile(request, username):
     person = get_object_or_404(get_user_model(), username = username )
     post_list = Post.objects.order_by('-create_date')
-    post_photo = Post.objects.filter(author_id = person.id)
+    post_photo = Post.objects.filter(author_id = person.id).order_by('-create_date')
+    # post_photo.order_by('-create_date')
+    # post_list.filter(author_id = person.id)
     # post_photo = Post.objects.filter(author_id = request.user.id)
     
     context = {
