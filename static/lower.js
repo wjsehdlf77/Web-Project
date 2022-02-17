ad40b795119854f515ba2b21d60e1f002012f5b7
JHJ <90052524+quantumleapj@users.noreply.github.com>
2022-02-17 21:27:31 +0900

    좋아요 개수

--- static/lower.js
+++ static/lower.js
@@ -12,28 +12,8 @@ function display_like() {
     let checkbox_label = document.getElementById(id_str);
     let checkbox = checkbox_label.getElementsByTagName("input")[0];
     let checkbox_in = checkbox_label.getElementsByTagName("span")[0];
-
-
     checkbox.addEventListener("change", function (e) {
-    
-      // console.log(this)
-      let pe = this.parentElement.parentElement
-      console.log(pe)
-      // let div_count = $('feed_txt')
-      let div_count=pe.getElementsByClassName('feed_txt')
-      console.log(div_count)
-      let content = div_count.item(0)
-      // console.log(div_count.innerHTML)
-    
-      // let content = div_count.textContent
-      // let content = div_count.data()
-      console.log(content)
-      console.log(typeof(content))
-      // console.log(content.textContent)
-      content = content.textContent
 
-      c_txt=content.substr(3,content.length)
-      console.log(c_txt)
 
       if (checkbox.checked) {
         checkbox_in.innerHTML = `<img src="/static/images/cloud_full.png" class="like_icon" alt="" />`;
@@ -90,6 +70,47 @@ function display_like() {
           },
         });
       }
+      
+
+      // console.log(e)
+      let pe = this.parentElement.parentElement
+      // console.log(pe)
+      let div_count=pe.getElementsByClassName('feed_txt')
+      let count_id= div_count[0].getAttribute('id')
+      console.log(count_id)
+      let content = div_count.item(0)
+      // console.log(content)
+      // console.log(typeof(content))
+      content = content.textContent
+
+      c_txt=content.substr(3,content.length)
+      c_txt=c_txt.substr(0,c_txt.length-1)
+      // console.log(c_txt)
+      c_txt=(parseInt(c_txt)+1).toString()
+      let temp_html = '<b>좋아요 '+c_txt+'개</b>'
+      // div_count.innerHTML=temp_html
+      // div_count.innerText=temp_html
+      console.log(div_count)
+      like_c=$("#"+count_id)
+      like_c.empty()
+      like_c.append(temp_html)
+
+
+
+      // window.div_count.reload()
+      // $('.feed_txt').load(location.href+' .feed_txt');
+      // div_count.load(location.href+' .feed_txt')
+      // div_count.     
+      // $('.feed_txt').load($('.feed_txt').value)
+      // div_count.load(location.href+' .feed_txt')
+      // $('.feed_txt').load(location.href+' .feed_txt[0]');
+      // $('.feed_txt').load(location.href);//엄청난 오류...
+      // $('#'+count_id).load(location.href+' #'+count_id);
+      // console.log(typeof(div_count))
+      // console.log(typeof($('.feed_txt')))
+      // console.log($('.feed_txt'))
+      // console.log(div_count)
+      console.log($("#"+count_id))
       // location.reload()
     });
     // checkbox_label.addEventListener("click", function () {
